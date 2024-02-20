91b8cc37d9221d54e51511269fcb16c3b951cdeb
Masashi Takanobu <32604391+matakanobu@users.noreply.github.com>
2024-02-20 16:59:07 +0900

    chore: test (#5)

--- projects/project-a/tests/test_pipeline.py
+++ projects/project-a/tests/test_pipeline.py
@@ -8,3 +8,15 @@ def test_pipeline_run(capsys):
     expected = "Running pipeline A\n"
 
     assert actual == expected
+
+
+def test_pipeline_preprocess(capsys):
+    from src.pipeline import Pipeline
+
+    pipeline = Pipeline()
+    pipeline.preprocess()
+
+    actual = capsys.readouterr().out
+    expected = "Preprocessing data\n"
+
+    assert actual == expected
